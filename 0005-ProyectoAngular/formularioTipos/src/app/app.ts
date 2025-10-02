08f298bca08b9ed7934c95416b5a04890e6ead49
KenVeraChan <kenveracha@gmail.com>
2025-10-03 00:26:09 +0200

    Mejoras en directivas

--- 0005-ProyectoAngular/formularioTipos/src/app/app.ts
+++ 0005-ProyectoAngular/formularioTipos/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Elemento } from './elementos.model';
+import { Personas } from './elementos.model';
+import { Libros } from './elementos.model';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,60 @@ import { Elemento } from './elementos.model';
   styleUrl: './app.css'
 })
 export class App {
-  titulo = 'listadoElementos';
+  tituloGeneral='Listas Variadas'
+  tituloElementos = 'lista de Frutas y Verduras';
+  tituloPersonas = 'Lista de los Escritores presentados';
+  tituloLibros= 'Lista de los libros de la Biblioteca'
   elementos: Elemento[] = [
     new Elemento("Manzanas", 5, 1.20),
     new Elemento("Naranjas", 3, 0.80),
     new Elemento("Plátanos", 6, 1.10)
   ];
+  personas: Personas[]=
+  [
+    new Personas("Juan", "Pérez", 30, "Ingeniero"),
+    new Personas("María", "Gómez", 25, "Diseñadora"),
+    new Personas("Carlos", "López", 40, "Profesor")
+  ];
+  libros: Libros[]=
+  [
+    new Libros("Cien Años de Soledad", "Gabriel García Márquez", 1967, "Realismo Mágico", "978-3-16-148410-0"),
+    new Libros("1984", "George Orwell", 1949, "Distopía", "978-0-452-28423-4"),
+    new Libros("El Gran Gatsby", "F. Scott Fitzgerald", 1925, "Novela", "978-0-7432-7356-5")
+  ];
+  categoriasElegidas: string[]=["elementos","personas","libros"];
+
+// Variables para el formulario
+  cuadroNombre: string = "";
+  cuadroCantidad: number = 0;
+  cuadroPrecio: number = 0; 
+  indiceMostrar: number=0;
+  
+  public agregarElemento()
+  {
+    if(this.cuadroCantidad > 0 && this.cuadroPrecio > 0) {
+      const nuevoElemento = new Elemento(this.cuadroNombre,this.cuadroCantidad,this.cuadroPrecio);
+      this.elementos.push(nuevoElemento);
+    } 
+  }
+  public setIndiceMostrado(indice: number)
+  {
+    this.indiceMostrar = indice;
+  }
+  public getIndiceMostrado()
+  {
+    return this.indiceMostrar;
+  }
+  public mostrarValor()
+  {
+    if(document.getElementById("selector")===null)
+    {
+      this.indiceMostrar=0;
+    }
+    else
+    {
+      const elemento = document.getElementById("selector") as HTMLSelectElement;
+      this.indiceMostrar = Number(elemento.value);
+    }
+  }
 }
- 
--- 0005-ProyectoAngular/formularioTipos/src/app/elementos.model.ts
+++ 0005-ProyectoAngular/formularioTipos/src/app/elementos.model.ts
@@ -2,10 +2,40 @@ export class Elemento{
     nombre: string="";
     cantidad: number=0;
     precio: number=0;
+    total: number=0.0;
     
     constructor(nombre: string, cantidad: number, precio: number){
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.precio = precio;
+        this.total= this.cantidad*this.precio;
+    }
+}
+export class Personas{
+    nombre: string="";
+    apellido: string="";
+    edad: number=0;
+    profesion: string="";
+
+    constructor(nombre: string, apellido: string, edad: number, profesion: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.profesion = profesion;
+    }
+}
+export class Libros{
+    titulo: string="";
+    autor: string="";
+    anio: number=0;
+    genero: string="";
+    isbn: string="";
+    
+    constructor(titulo: string, autor: string, anio: number, genero: string, isbn: string){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.genero = genero;
+        this.isbn = isbn;
     }
 }
